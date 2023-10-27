d892d35d9f3bed98b863e51a116ca80410c0b63a
Nazareno Bregante <tobiasbregante2018@gmail.com>
2023-10-27 16:52:14 -0300

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,27 +15,27 @@ export default function Web() {
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta name="twitter:card" content="summary_large_image" />
-        <title>Next.js Enterprise Boilerplate</title>
+        <title>MERCOSUR Shipping</title>
       </Head>
-      <section className="bg-white dark:bg-gray-900">
-        <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
+      <section className="containerTitleHome bg-white dark:bg-gray-900">
+        <div className="childContain mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
             <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
-              Next.js Enterprise Boilerplate
+              MERCOSUR Shipping
             </h1>
             <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
-              Jumpstart your enterprise project with our feature-packed, high-performance Next.js boilerplate!
-              Experience rapid UI development, AI-powered code reviews, and an extensive suite of tools for a smooth and
-              enjoyable development process.
+              En MERCOSUR Shipping, somos especialistas en hacer que la importación de tus productos sea fácil y 
+              segura. Con una variedad de servicios de transporte, estamos listos para satisfacer todas tus necesidades 
+              logísticas.
             </p>
-            <Button href="https://github.com/Blazity/next-enterprise" className="mr-3">
-              Get started
+            <Button href={`https://api.whatsapp.com/send?text=Hola, desde MERCOSUR Shipping! Quiero contactarme con un operador!&phone=5491124767008`} className="mr-3">
+              Contactar
             </Button>
             <Button
-              href="https://vercel.com/new/git/external?repository-url=https://github.com/Blazity/next-enterprise"
+              href={`https://api.whatsapp.com/send?text=Hola, desde MERCOSUR Shipping! Quiero un presupuesto!&phone=5491124767008`}
               intent="secondary"
             >
-              Deploy Now
+              Solicitar presupuesto
             </Button>
           </div>
         </div>
